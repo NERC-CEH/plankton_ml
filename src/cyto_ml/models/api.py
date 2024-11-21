1e41b3708b2b9c621a095468e8888ed4f63ce62b
Jo Walsh <jowals@ceh.ac.uk>
2024-11-21 15:14:59 +0000

    Outline for a minimal FastAPI interface to range of models

---
+++ src/cyto_ml/models/api.py
@@ -0,0 +1,7 @@
+# Experimental API for serving a range of plankton models
+# * Choose model endpoint
+# * POST image contents
+# * Probably parameter for a normalisation function with a sensible default
+# * Get back a dict with classification
+# * Option for confidence levels (if our models are calibrated)
+# * Option to also return embeddings (could be enabled by default)

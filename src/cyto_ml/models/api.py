f2ec6241cd88baf944b01f5c14b1744ababbef9e
Jo Walsh <jowals@ceh.ac.uk>
2024-11-22 14:24:03 +0000

    load models as globals when worker starts, see comments

--- src/cyto_ml/models/api.py
+++ src/cyto_ml/models/api.py
@@ -12,7 +12,6 @@ import uvicorn
 from fastapi import FastAPI, Form
 from fastapi.responses import JSONResponse
 from resnet50_cefas import load_model
-from torchvision import Module
 
 from cyto_ml.data.image import load_image_from_url
 from cyto_ml.data.labels import RESNET18_LABELS
@@ -20,17 +19,38 @@ from cyto_ml.models.utils import flat_embeddings, resnet18
 
 STATE_FILE = "../../../data/weights/ResNet_18_3classes_RGB.pth"
 
+# 3-class ResNet18, newer work from Turing Inst
+# https://noushineftekhari.github.io/publication/2024-marine-plankton-classification
+resnet18_classifier = None
+resnet18_embeddings = None
 
-# TODO pass state in a reproducible way - weights are on Google Drive
-# TODO is there a cache decorator like streamlit's for this
-def resnet18_model(num_classes: int = 3, strip_final_layer: bool = False) -> Module:
-    state_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), STATE_FILE)
-    return resnet18(num_classes=num_classes, filename=state_file, strip_final_layer=strip_final_layer)
-
+# Fork of earlier Turing model via sci.vision
+# https://github.com/ukceh-rse/resnet50-cefas
+resnet50_model = None
 
 app = FastAPI()
 
-# TODO cache models rather than load every time
+# TODO look at ProcessPoolExecutor with load function for concurrent requests
+# https://luis-sena.medium.com/how-to-optimize-fastapi-for-ml-model-serving-6f75fb9e040d
+# The load_models function here is made with that in mind.
+
+
+# TODO pass state in a reproducible way - weights are on Google Drive
+# This could easily become overkill if we start adding ViT, BioCLIP etc
+def load_models() -> None:
+    global resnet18_classifier  # noqa PLW0603
+    state_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), STATE_FILE)
+    resnet18_classifier = resnet18(num_classes=3, filename=state_file)
+
+    global resnet18_embeddings  # noqa PLW0603
+    state_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), STATE_FILE)
+    resnet18_embeddings = resnet18(num_classes=3, filename=state_file, strip_final_layer=True)
+
+    global resnet50_model  # noqa PLW0603
+    resnet50_model = load_model(strip_final_layer=True)
+
+
+load_models()
 
 
 @app.get("/")
@@ -44,25 +64,23 @@ async def root() -> JSONResponse:
 @app.post("/resnet50/")
 async def resnet50(url: str = Form(...)) -> JSONResponse:
     # strip_final_layer is only if we want embeddings
-    model = load_model(strip_final_layer=True)
-    features = model(load_image_from_url(url))
+
+    features = resnet50_model(load_image_from_url(url))
     embeddings = flat_embeddings(features)
     return {"embeddings": embeddings}
 
 
 @app.post("/resnet18/")
 async def resnet18_3(url: str = Form(...)) -> JSONResponse:
-    # Use the 3 class Resnet18 model to return a prediction
-    # TODO load into session state in a correct way
-    model = resnet18_model()
+    """Use the 3 class Resnet18 model to return both a prediction
+    and a set of image embeddings"""
+
     image = load_image_from_url(url)
     # TODO look at the normalisation / resize functions in Vit-lasnet tests, use them?
-    outputs = model(image)
+    outputs = resnet18_classifier(image)
     _, predicted = torch.max(outputs, 1)
 
-    # See TODOs above about loading these once
-    embeddings_model = resnet18_model(strip_final_layer=True)
-    outputs = embeddings_model(image)
+    outputs = resnet18_embeddings(image)
     embeddings = flat_embeddings(outputs)
 
     return {"classification": RESNET18_LABELS[predicted], "embeddings": embeddings}

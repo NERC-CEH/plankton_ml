6fa901777d11c87da1ebd391356c003f2d16e845
Jo Walsh <jo@debian.ceh.ac.uk>
2025-01-29 10:52:33 +0000

    simplify indexing scripts with s3fs, local output for now

--- scripts/image_metadata.py
+++ scripts/image_metadata.py
@@ -1,22 +1,32 @@
-"""Create a basic index for the images in an s3 collection
-"""
+"""Create a basic index for the images in an s3 collection"""
 
 import yaml
-from cyto_ml.data.s3 import boto3_client, image_index
+import logging
+import os
+import s3fs
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+load_dotenv()
+
+
+def bucket_listing(bucket_name: str):
+    """Create an index of images in a bucket,"""
+    s3 = s3fs.S3FileSystem(anon=True, endpoint_url=os.environ["AWS_URL_ENDPOINT"])
+
+    # This returns paths with bucket name pre-pended
+    # E.g. 'untagged-images-lana/MicrobialMethane_MESO_Tank7_54.0143_-2.7770_18052023_1_17085.tif'
+    contents = s3.ls(bucket_name)
+
+    return [i.split("/")[-1] for i in filter(lambda x: x.endswith("tif"), contents)]
 
 
 if __name__ == "__main__":
+    # Expects the bucket name set as "collection" in params.yml (used by DVC)
 
-    # Write a minimal CSV index of images in a bucket
-    # Was originally part of an intake catalogue setup
-    image_bucket = yaml.safe_load(open("params.yaml"))["collection"]
+    bucket_name = yaml.safe_load(open("params.yaml"))["collection"]
 
-    metadata = image_index(image_bucket)
-
-    s3 = boto3_client()
-
-    catalog_csv = metadata.to_csv(index=False)
-    with open("catalog.csv", "w") as out:
-        out.write(catalog_csv)
-
-    s3.put_object(Bucket=image_bucket, Key="catalog.csv", Body=catalog_csv)
+    images = bucket_listing(bucket_name)
+    # Increment - keep a flat file index locally
+    with open(f"{bucket_name}.csv", "w") as out:
+        out.write("\n".join(images))
